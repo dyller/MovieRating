2224a86da011fdd159b02f61c25c008ec8ffc934
SamPerCaparros <samu1667@easv365.dk>
2018-10-26 12:43:17 +0200

    Little update with questions

--- MovieRating/Executable/Program.cs
+++ MovieRating/Executable/Program.cs
@@ -4,7 +4,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Threading;
 
 namespace Executable
 {
@@ -12,28 +11,9 @@ namespace Executable
     {
         static void Main(string[] args)
         {
-            using (StreamReader r = new StreamReader("C:/Users/Samuel/Downloads/ratings.json"))
-            {
-                var json = r.ReadToEnd();
-                List<Reviews> list = JsonConvert.DeserializeObject<List<Reviews>>(json);
-                Console.WriteLine("Deserialization finished.");
-
-                Console.WriteLine("Do you want start taking shits?");
-                string val = Console.ReadLine();
-                if (val == "Yes")
-                {
-                    foreach (var item in list.Where(w => w.Reviewer == 1))
-                    {
-                        Console.WriteLine("Reviewer " + item.Reviewer + "Movie " + item.Movie);
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("You decided to dont");
-                }
-                
-            }
-            Console.ReadLine();
+            var mD = new MovieData();
+            mD.JustBasic();
         }
+
     }
 }
--- MovieRating/MovieRating/MovieData.cs
+++ MovieRating/MovieRating/MovieData.cs
@@ -2,13 +2,41 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 
 namespace MovieRating
 {
-    class MovieData : IMovieData
+    public class MovieData : IMovieData
     {
-        
+        public void JustBasic()
+        {
+            using (StreamReader r = new StreamReader("C:/Users/Samuel/Downloads/ratings.json"))
+            {
+                Console.WriteLine("Reading File...");
+                var json = r.ReadToEnd();
+                List<Reviews> list = JsonConvert.DeserializeObject<List<Reviews>>(json);
+                Console.WriteLine("Deserialization finished.");
+
+                Console.WriteLine("Do you want start taking shits?");
+                string val = Console.ReadLine();
+                if (val == "Yes")
+                {
+                    Console.WriteLine("Write");
+                    int number = Convert.ToInt32(Console.ReadLine());
+                    foreach (var item in list.Where(w => w.Reviewer == number))
+                    {
+                        Console.WriteLine("Reviewer " + item.Reviewer + "Movie " + item.Movie);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("You decided to dont");
+                }
+
+            }
+            Console.ReadLine();
+        }
         public double AverageGrade(double Reviewer)
         {
             throw new NotImplementedException();
